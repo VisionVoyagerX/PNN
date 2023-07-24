d049622c79f0f83c5b41f5926f5fb31deb7792c1
nickdndndn <nickd_96@hotmail.com>
2023-07-24 17:54:43 +0300

    report

--- evaluate.py
+++ evaluate.py
@@ -20,23 +20,23 @@ def main():
     print(device)
 
     # Initialize DataLoader
-    train_dataset = WV3(
-        Path("/home/ubuntu/project/Data/WorldView3/train/train_wv3-001.h5"), transforms=[(RandomHorizontalFlip(1), 0.3), (RandomVerticalFlip(1), 0.3)])  # /home/ubuntu/project
+    train_dataset = GaoFen2(
+        Path("F:/Data/GaoFen-2/train/train_gf2-001.h5"), transforms=[(RandomHorizontalFlip(1), 0.3), (RandomVerticalFlip(1), 0.3)])  # /home/ubuntu/project
     train_loader = DataLoader(
         dataset=train_dataset, batch_size=128, shuffle=True, drop_last=True)
 
-    validation_dataset = WV3(
-        Path("/home/ubuntu/project/Data/WorldView3/val/valid_wv3.h5"))
+    validation_dataset = GaoFen2(
+        Path("F:/Data/GaoFen-2/val/valid_gf2.h5"))
     validation_loader = DataLoader(
         dataset=validation_dataset, batch_size=16, shuffle=True)
 
-    test_dataset = WV3(Path(
-        "/home/ubuntu/project/Data/WorldView3/drive-download-20230627T115841Z-001/test_wv3_multiExm1.h5"))
+    test_dataset = GaoFen2(
+        Path("F:/Data/GaoFen-2/drive-download-20230623T170619Z-001/test_gf2_multiExm1.h5"))
     test_loader = DataLoader(
         dataset=test_dataset, batch_size=8, shuffle=False)
 
     # Initialize Model, optimizer, criterion and metrics
-    model = PNNmodel(scale=4, ms_channels=8, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
+    model = PNNmodel(scale=4, ms_channels=4, mslr_mean=train_dataset.mslr_mean.to(device), mslr_std=train_dataset.mslr_std.to(device), pan_mean=train_dataset.pan_mean.to(device),
                      pan_std=train_dataset.pan_std.to(device)).to(device)
 
     my_list = ['conv_3.weight', 'conv_3.bias']
@@ -84,7 +84,7 @@ def main():
     evaluation_interval = [10000, 200000, 300000, 400000,
                            500000, 600000, 700000, 800000, 900000, 1000000]
     val_steps = 50
-    continue_from_checkpoint = False
+    continue_from_checkpoint = True
 
     summary(model, [(1, 1, 256, 256), (1, 4, 64, 64)],
             dtypes=[torch.float32, torch.float32])
@@ -92,10 +92,10 @@ def main():
     # load checkpoint
     if continue_from_checkpoint:
         tr_metrics, val_metrics, test_metrics = load_checkpoint(torch.load(
-            'checkpoints/pnn_model_WV3/pnn_model_2023_07_17-11_30_23_best_eval.pth.tar'), model, optimizer, tr_metrics, val_metrics, test_metrics)
+            'checkpoints/pnn_model/pnn_model_2023_07_17-11_30_23.pth.tar'), model, optimizer, tr_metrics, val_metrics, test_metrics)
         print('Model Loaded ...')
 
-    print('==> Starting training ...')
+    """print('==> Starting training ...')
     train_iter = iter(train_loader)
     train_progress_bar = tqdm(iter(range(steps)), total=steps, desc="Training",
                               leave=False, bar_format='{desc:<8}{percentage:3.0f}%|{bar:15}{r_bar}')
@@ -211,59 +211,41 @@ def main():
                               'test_metrics': test_metrics}
                 save_checkpoint(checkpoint, 'pnn_model_WV3',
                                 current_daytime + '_best_eval')
+"""
+    # test model
 
-        # test model
-        if (step + 1) in test_intervals and step != 0:
-            # evaluation mode
-            model.eval()
-            with torch.no_grad():
-                print("\n==> Start testing ...")
-                test_progress_bar = tqdm(iter(test_loader), total=len(
-                    test_loader), desc="Testing", leave=False, bar_format='{desc:<8}{percentage:3.0f}%|{bar:15}{r_bar}')
-                for pan, mslr, mshr in test_progress_bar:
-                    # forward
-                    pan, mslr, mshr = pan.to(device), mslr.to(
-                        device), mshr.to(device)
-                    mssr = model(pan, mslr)
-                    test_loss = criterion(mssr, mshr)
-                    test_metric = test_metric_collection.forward(mssr, mshr)
-                    test_report_loss += test_loss
+    model.eval()
+    with torch.no_grad():
+        print("\n==> Start testing ...")
+        test_progress_bar = tqdm(iter(test_loader), total=len(
+            test_loader), desc="Testing", leave=False, bar_format='{desc:<8}{percentage:3.0f}%|{bar:15}{r_bar}')
+        for pan, mslr, mshr in test_progress_bar:
+            # forward
+            pan, mslr, mshr = pan.to(device), mslr.to(
+                device), mshr.to(device)
+            mssr = model(pan, mslr)
+            test_loss = criterion(mssr, mshr)
+            test_metric = test_metric_collection.forward(mssr, mshr)
+            test_report_loss += test_loss
 
-                    # report metrics
-                    test_progress_bar.set_postfix(
-                        loss=f'{test_loss.item()}', psnr=f'{test_metric["psnr"].item():.2f}', ssim=f'{test_metric["ssim"].item():.2f}')
+            # report metrics
+            test_progress_bar.set_postfix(
+                loss=f'{test_loss.item()}', psnr=f'{test_metric["psnr"].item():.2f}', ssim=f'{test_metric["ssim"].item():.2f}')
 
-                # compute metrics total
-                test_report_loss = test_report_loss / len(test_loader)
-                test_metric = test_metric_collection.compute()
-                test_metrics.append({'loss': test_report_loss.item(),
-                                     'psnr': test_metric['psnr'].item(),
-                                     'ssim': test_metric['ssim'].item()})
+        # compute metrics total
+        test_report_loss = test_report_loss / len(test_loader)
+        test_metric = test_metric_collection.compute()
+        test_metrics.append({'loss': test_report_loss.item(),
+                             'psnr': test_metric['psnr'].item(),
+                             'ssim': test_metric['ssim'].item()})
 
-                print(
-                    f'\nTesting: avg_loss = {test_report_loss.item():.4f} , avg_psnr= {test_metric["psnr"]:.4f}, avg_ssim={test_metric["ssim"]:.4f}')
+        print(
+            f'\nTesting: avg_loss = {test_report_loss.item():.4f} , avg_psnr= {test_metric["psnr"]:.4f}, avg_ssim={test_metric["ssim"]:.4f}')
 
-                # reset metrics
-                test_report_loss = 0
-                test_metric_collection.reset()
-                print("==> End testing <==\n")
-
-            # train mode
-            model.train()
-
-            # save best test model based on PSNR
-            if test_metrics[-1]['psnr'] > best_test_psnr:
-                best_test_psnr = test_metrics[-1]['psnr']
-                checkpoint = {'step': step,
-                              'state_dict': model.state_dict(),
-                              'optimizer': optimizer.state_dict(),
-                              'tr_metrics': tr_metrics,
-                              # 'val_metrics': val_metrics,
-                              'test_metrics': test_metrics}
-                save_checkpoint(checkpoint, 'pnn_model_WV3',
-                                current_daytime + '_best_test')
-
-    print('==> training ended <==')
+        # reset metrics
+        test_report_loss = 0
+        test_metric_collection.reset()
+        print("==> End testing <==\n")
 
 
 if __name__ == '__main__':
