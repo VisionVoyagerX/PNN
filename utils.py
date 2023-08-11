b623275f6dae1a2f727f8c770e190c4ba6126787
nickdndndn <nickd_96@hotmail.com>
2023-08-11 12:16:06 +0300

    report

--- utils.py
+++ utils.py
@@ -50,14 +50,11 @@ def save_checkpoint(state, filename: str, current_daytime: str):
 
 def load_checkpoint(checkpoint, model, optimizer, tr_metrics, val_metrics, test_metrics):
     # print("=> Loading checkpoint")
-    try:
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        tr_metrics = checkpoint['tr_metrics']
-        val_metrics = checkpoint['val_metrics']
-        test_metrics = checkpoint['test_metrics']
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    tr_metrics = checkpoint['tr_metrics']
+    val_metrics = checkpoint['val_metrics']
+    test_metrics = checkpoint['test_metrics']
 
-        return (tr_metrics, val_metrics, test_metrics)  # val_metrics
-    except:
-        print('Could not load model')
-        return Exception
+    return (tr_metrics, val_metrics, test_metrics)  # val_metrics
+   
